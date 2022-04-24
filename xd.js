7305bf3521563a8acb2b4de992162bbb4b25322a
Felyp <janichfilip@seznam.cz>
2022-04-25 01:39:31 +0200

    Add files via upload

---
+++ xd.js
@@ -0,0 +1,54 @@
+const tmi = require('tmi.js'),
+{ username, password } = require('./settings.json');
+const client = new tmi.Client({    
+
+    options: { 
+        joinInterval: 300,
+        debug: true, 
+        messagesLogLevel: "info"
+    },
+    connection: {
+        reconnect: true,
+        secure: true
+    },
+    identity: {
+        username: username,
+        password: password
+    },
+    channels: ["YOUR_CHANNELS"]
+});
+
+client.connect().catch(console.error);
+
+
+
+    client.on("message", async (channel, user, message, self) => { 
+
+        
+
+
+        let isMod = user.mod || user['user-type'] === 'mod';
+        let isBroadcaster = channel.slice(1) === user.username;
+        let xd = isMod || isBroadcaster;
+        let isBroadcasterUp = isBroadcaster;
+
+
+        const args = message.slice(1).split(' ')
+        
+
+
+        
+        if (message.toLowerCase().startsWith('_spam') && user.username === username){
+            
+            for (let i=0;i<120;i++) {
+                client.say(channel, args.slice(1).join(" "))
+            
+                }
+            } 
+        
+        
+
+        
+        
+
+})
